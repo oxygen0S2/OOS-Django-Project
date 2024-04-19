8d2ed4a70e00dee41bc4bd8c4c4a0f6dcccfc465
jiyoung <0oxgen2@naver.com>
2024-04-20 02:51:34 +0900

    수정

--- todolist_app/todolist_app/urls.py
+++ todolist_app/todolist_app/urls.py
@@ -20,6 +20,6 @@ from django.urls import path
 from app.views import index
 
 urlpatterns = [
-    path("index.html/", index, name="index"),
+    path('index.html/', index, name='index'),
     path('admin/', admin.site.urls),
 ]
